b6c81766a18a138ced21b20a2f78cda4504870dc
haydencarroll1 <carrollhayden8@gmail.com>
2023-03-24 11:05:56 +0000

    commit

--- Year1/Semester 2/Week 7/Lab/Q1.c
+++ Year1/Semester 2/Week 7/Lab/Q1.c
@@ -1,10 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define SIZE 10
 
 int main()
 {
-    char *names[SIZE] = {}
+    char *names[SIZE] = {"Hayden", "Sam", "James", "Peter", "Michael", "Eoin", "Jamie", "Oisin", "Oscar", "Adam"};
+    char user_input[SIZE];
+    int result = 0;
+    int length;
+
+    printf("Please enter your name and I will check to see if it's in our database:");
+    scanf("%s", user_input);
+
+    length = strlen(user_input);
+
+    for(int i = 0; i < SIZE; i++)
+    {
+        result = strcmp(user_input, names[i]);
+
+        if(result != 0 && i == (SIZE - 1))
+        {
+            printf("Name not found");
+            break;
+        }
+        else if(result == 0)
+        {
+            printf("\nWelome back %s!\n", names[i]);
+            break;
+        }
+    }
 
 
     return 0;
